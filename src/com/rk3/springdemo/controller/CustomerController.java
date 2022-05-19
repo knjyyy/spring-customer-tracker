af0c45ac42d66149cbdf13c20434cacf0df40d7a
Kenji Trinidad <rzzlknjtrndd@gmail.com>
2022-05-19 20:07:22 +0800

    Initial commit

---
+++ src/com/rk3/springdemo/controller/CustomerController.java
@@ -0,0 +1,15 @@
+package com.rk3.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+
+	@RequestMapping("/list")
+	public String listCustommer(Model model) {
+		return "list-customers";
+	}
+}
